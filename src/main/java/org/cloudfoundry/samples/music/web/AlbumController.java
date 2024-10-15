354c6157372d7c713a5d8147191664bdc8a3a0cc
soyoung leee <134457816+soyoung1122@users.noreply.github.com>
2024-10-15 14:07:52 +0900

    Update AlbumController.java

--- src/main/java/org/cloudfoundry/samples/music/web/AlbumController.java
+++ src/main/java/org/cloudfoundry/samples/music/web/AlbumController.java
@@ -49,7 +49,7 @@ public class AlbumController {
         repository.deleteById(id);
     }
 
-    @RequestMapping(value = "/testdd", method = RequestMethod.GET)
+    @RequestMapping(value = "/testdggggd", method = RequestMethod.GET)
     public String testController() {
         logger.info("jenkins test");
         return "testtesttesttest";
